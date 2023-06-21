730569810917fc6a08c6cc83223d80e58c879d63
YOUNGEUN SUH <121986519+YOUNGEUN100@users.noreply.github.com>
2023-06-22 08:27:26 +0900

    update

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# weather-app
-리액트로 만든 날씨앱
-## 홈페이지
-https://frabjous-naiad-fed6b7.netlify.app/
+# wise-saying-app
+- 리액트로 만든 명언 앱니다. 
+## 캡쳐화면
+-- pc
+<img src="https://github.com/YOUNGEUN100/wise-saying-app/assets/121986519/7f24bee4-b149-4463-9e6d-7c9ffa5342e9">
+-- mobile
+<img src="https://github.com/YOUNGEUN100/wise-saying-app/assets/121986519/61897bca-430f-4f51-96cb-ac9d2d33b6c5">
+## homepage
+- https://incandescent-elf-f6ce17.netlify.app/
+## 기능
+- 명언 텍스트가 5초마다 변경됨
+- 요일-월-일 표시
+- 더보기 버튼 클릭시 명언자의 검색어 창이 뜸
+
+
+
+
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,44 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import Search from './components/Search';
-import Info from './components/Info';
-import Today from './components/Today';
+import {random} from "kadvice";
 
 function App() {
-  const API_KEY = import.meta.env.VITE_API_KEY;
+  // 명언 랜덤하게 가져오기
+  const [advice, setAdvice] = useState(random());
 
-  const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState(null);
-  const [message, setMessage] = useState("");
- // const [image, setImage] = useState("");
+  useEffect(()=>{
+    let adviceInterval = setInterval(()=>{
+      setAdvice(random());
+    }, 5000);
+    return () => clearInterval(adviceInterval);
+  },[])
 
-  const handleLocationChange = (e) => {
-    setLocation(e.target.value);
-    // console.log(location);
-  };  
+  // 오늘 날짜 가져오기
+  const days = ["일","월","화","수","목","금","토"];
+  const d = new Date();
+  let day = days[d.getDay()];
+  const month = d.getMonth() + 1;
+  const date = d.getDate();
 
-  const fetchWeather = () => {
-    const apiKey = API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&lang=kr`;
+  // '더보기' 누르면 구글 검색으로 이동
+  const moreInfo = () => {
+    window.open(`https://www.google.com/search?q=${advice.author}&hl=ko&sxsrf=APwXEdeeqCVSISx2YNxTQxZxb8GhEGUlhA%3A1687249773557&source=hp&ei=bWORZLTWH8OR-AbGma3wCQ&iflsig=AOEireoAAAAAZJFxffyd6K9sjB5TApPxiyBrvce3q8PT&ved=0ahUKEwj0wJebt9H_AhXDCN4KHcZMC54Q4dUDCAs&uact=5&oq=%EC%98%A4&gs_lcp=Cgdnd3Mtd2l6EAMyBwgjEIoFECcyBwgjEIoFECcyBwgjEIoFECcyBAgAEAMyEQguEIAEELEDEIMBEMcBENEDMgsIABCABBCxAxCDATILCAAQgAQQsQMQgwEyEQguEIAEELEDEIMBEMcBENEDMgUILhCABDILCAAQgAQQsQMQgwE6BwgjEOoCECc6BQgAEIAEUOsBWI0MYM0NaANwAHgAgAGBAYgB8wGSAQMwLjKYAQCgAQGwAQo&sclient=gws-wiz`)
+  }
 
-    fetch(url)
-      .then((res)=>res.json())
-      .then((data) => {
-        if (data.cod === "404") {
-          setWeather(null);
-          setMessage("올바른 지역명이 아닙니다");
-          return;
-        }
-        setWeather(data);
-        console.log(weather);
-      });
-  };
-
-  
-
-  const handleWeatherSearch = (e) => {
-    e.preventDefault();
-    fetchWeather();
-};
 
   return (
-    <div className="app">
-      <h1>Weather App</h1>
-      <Today />
-      <Search 
-        handleWeatherSearch={handleWeatherSearch}
-        location={location}
-        handleLocationChange={handleLocationChange}
-        />
-      <Info weather={weather} message={message}/>
+    <div className='App'>
+      <div className='now'>
+        <div className='dayST'>{day}요일</div>
+        <div className='today'>{month} / {date}</div>
       </div>
-  );
+      <div className='content'>
+        <div>{advice.message}</div>
+        <div>-{advice.author}</div>
+      </div>
+      <button className='moreBtn' onClick={moreInfo}>더보기</button>
+    </div>
+  )
 }
 
 export default App
