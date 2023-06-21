bf85d4498ce9445ee47799d5b9b38ec9803f87c0
YOUNGEUN SUH <121986519+YOUNGEUN100@users.noreply.github.com>
2023-06-21 12:51:56 +0900

    second commit

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,8 +8,15 @@ function Info(props) {
      {weather ? (
         <div className="info">
           <div>{weather.name}</div>
-          <div>현재온도: {weather.main.temp} </div>
-          <div>설명: {weather.weather[0].description}</div>
+          <div>현재온도: {(weather.main.temp - 273.15).toFixed(1)}℃ </div>
+          <div>
+             <span>최저온도 : {(weather.main.temp_min - 273.15).toFixed(1)}℃</span>
+             <span>/ 최고온도 : {(weather.main.temp_max - 273.15).toFixed(1)}℃</span> 
+          </div>
+          <div className='des'>
+            <span>설명: {weather.weather[0].description}</span>
+            <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}/>
+          </div>
       </div>) : message? 
       <div className='msg'>{message}</div> : null}   
     </>
